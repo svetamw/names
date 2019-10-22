4e2fcecb16bf7a2db90edb5baf06a0c4d11a9a6e
stud <stud@RUDN-422-6>
2019-10-22 11:24:35 +0300

    output files All_students, Best and Last

--- names/Program.cs
+++ names/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-
+using System.Text;
 
 namespace names
 {
@@ -45,14 +45,26 @@ namespace names
             foreach (var stud in students)
             {
                 var average_rating = stud.AverageRating;
-                if (average_rating > max_treshold)
-                    best.Add(stud);
-                else if (average_rating < min_treshold)
-                    last.Add(stud);
-
+                {
+                    if (average_rating > max_treshold)
+                    {
+                        best.Add(stud);
+                            }
+                    else if (average_rating < min_treshold)
+                    {
+                        last.Add(stud);
+                            }
+                }
+                
+               
             }
-           // stud.Visualize();
-            Console.ReadLine();
+            Student.WriteToCSV("All_students.csv", students);
+            Student.WriteToCSV("Best_students.csv", best.ToArray());
+            Student.WriteToCSV("Last_students.csv", last.ToArray());
+
+
+            // stud.Visualize();
+            //Console.ReadLine();
         }
         static Student[] CreateStudents(string[] Names, string[] LastNames, string[] Patronymics, int Count)
         {
@@ -93,10 +105,60 @@ namespace names
 
     class Student
     {
-        public String FirstName;
-        public String LastName;
-        public String Patronymic;
+        /* public String FirstName;
+         public String LastName;
+         public String Patronymic;
+         public int[] Ratings;*/
+
+        public string FirstName, LastName, Patronymic;
         public int[] Ratings;
+
+        public Student(string LastName, string FirstName, string Patronymic)
+        {
+            this.LastName = LastName;
+            this.FirstName = FirstName;
+            this.Patronymic = Patronymic;
+        }
+
+        public Student()
+        {
+        }
+
+        public static void WriteToCSV(string FileName, Student[] Student)
+        {
+            using (var file = new StreamWriter(FileName, false, Encoding.UTF8))
+            {
+                file.WriteLine("LastName;FirstName;Patronymic;Ratings");
+                for (var i = 0; i < Student.Length; i++)
+                {
+                    var ratings = string.Join(";", Student[i].Ratings);
+                    file.WriteLine("{0};{1};{2};{3}", Student[i].LastName, Student[i].FirstName, Student[i].Patronymic, ratings);
+                }
+            }
+        }
+
+        public static Student[] ReadCSV(String FileName)
+        {
+            var result = new List<Student>(1000);
+            using (var file = new StreamReader(FileName))
+            {
+                file.ReadLine();
+                while (!file.EndOfStream)
+                {
+                    var line = file.ReadLine();
+                    var values = line.Split(';');
+                    var student = new Student(values[0], values[1], values[2]);
+                    var ratings = new int[values.Length - 3];
+                    for (var i = 0; i < ratings.Length; i++)
+                        ratings[i] = int.Parse(values[i + 3]);
+                    student.Ratings = ratings;
+                    result.Add(student);
+
+                }
+            }
+            return result.ToArray();
+        }
+
 
         public double AverageRating
         {
